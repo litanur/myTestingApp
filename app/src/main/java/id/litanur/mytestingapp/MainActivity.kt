f6ace609f96fd7ae29e7d6573b0c418799e725da
Lita Nurlaelati <litanurlaelati@gmail.com>
2022-07-31 21:42:48 +0700

    Initial commit

---
+++ app/src/main/java/id/litanur/mytestingapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package id.litanur.mytestingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+    private var names = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSetValue = findViewById(R.id.btn_set_value)
+        tvText = findViewById(R.id.tv_text)
+
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Narenda Wicaksono")
+        names.add("Kevin")
+        names.add("Yoza")
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_set_value) {
+            Timber.d("Data ditampilkan")
+            val name = StringBuilder()
+            for (i in 0..2) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
+    }
+}
